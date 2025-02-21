de9dd1ca21957e31d8f9833679323ac745a09136
Gabriel Park <40016456+gabrpark@users.noreply.github.com>
2025-02-20 20:28:13 -0800

    Remove the unused menu items

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -70,25 +70,25 @@ function DesktopNav() {
           <span className="sr-only">Acme Inc</span>
         </Link>
 
-        <NavItem href="#" label="Dashboard">
+        {/* <NavItem href="#" label="Dashboard">
           <Home className="h-5 w-5" />
-        </NavItem>
+        </NavItem> */}
 
-        <NavItem href="#" label="Orders">
+        {/* <NavItem href="#" label="Orders">
           <ShoppingCart className="h-5 w-5" />
-        </NavItem>
+        </NavItem> */}
 
         <NavItem href="/" label="Products">
           <Package className="h-5 w-5" />
         </NavItem>
 
-        <NavItem href="/customers" label="Customers">
+        {/* <NavItem href="/customers" label="Customers">
           <Users2 className="h-5 w-5" />
-        </NavItem>
+        </NavItem> */}
 
-        <NavItem href="#" label="Analytics">
+        {/* <NavItem href="#" label="Analytics">
           <LineChart className="h-5 w-5" />
-        </NavItem>
+        </NavItem> */}
       </nav>
       <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
         <Tooltip>
